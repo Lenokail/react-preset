4e9a47c25567a501a3924f4f78bbf579cb749892
Lenokail <vadim-vadim15@rambler.ru>
2022-07-06 13:26:59 +0500

    Init

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './App.scss'
+
+const App = () => {
+  return (
+	<div className={styles.App}>
+		Hello, world
+	</div>
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import App from './Components/App/App';
+
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+	<App/>
+);
+
+const devMode = process.env.NODE_ENV === 'development';
+if (devMode && module && module.hot) {
+	module.hot.accept();
+}
